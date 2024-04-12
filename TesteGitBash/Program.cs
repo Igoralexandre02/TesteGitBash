c0cb10be8bdf1abd7fb40317b72ce0bf0b46145e
Igor Alexandre <igorvieira@seduc.ro.gov.br>
2024-04-12 13:04:26 -0400

    Comeco do projeto

---
+++ TesteGitBash/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteGitBash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
