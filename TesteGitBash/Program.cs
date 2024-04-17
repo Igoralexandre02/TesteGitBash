e4e52fe2d1b74b4c99f8c33eb64ae89fd052f51f
Igor Alexandre <igorvieira@seduc.ro.gov.br>
2024-04-17 09:06:15 -0400

    Reformulado e adicionado uma funcao de divisao

--- TesteGitBash/Program.cs
+++ TesteGitBash/Program.cs
@@ -6,14 +6,29 @@ namespace TesteGitBash
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Good morning!");
-            Console.WriteLine("Qual seu nome?");
-            Console.WriteLine("Qual sua idade?");
-            Console.WriteLine("Carro");
-            Console.Write("Qual seu nome");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("");
+
+            Console.Write("Qual seu nome? ");
             string nome = Console.ReadLine();
-            Console.WriteLine("Bom dia" + nome);
+            Console.WriteLine("Bom dia, " + nome);
+
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("");
+            try
+            {
+                Console.WriteLine($"{nome}, informe dois número inteiro: ");
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+                int resultado = n1 / n2;
+                Console.WriteLine("A divisão dos números é: " + resultado);
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Erro! " + e.Message);
+            }
         }
     }
 }
